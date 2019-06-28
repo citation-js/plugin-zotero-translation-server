5a9987c18123ce9e2fb1843b4c5ef7660f0fa999
Lars Willighagen <lars.willighagen@gmail.com>
2019-06-29 00:16:09 +0200

    chore(travis): wait for server to start

---
+++ test/util/server.js
@@ -0,0 +1,15 @@
+const spawn = require('child_process').spawn
+
+switch (process.argv[2]) {
+  case 'start':
+    const server = spawn('npm', ['start'], {
+      detached: true,
+      windowsHide: true
+    })
+
+    server.stdout.on('data', function (data) {
+      if (data.indexOf('Listening on 0.0.0.0:1969') > -1) {
+        process.exit(0)
+      }
+    })
+}
